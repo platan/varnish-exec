471f31858533977e4f5c503d23f7b78a9bad53d0
Marcin Mielnicki <marcin.mielnicki@gmail.com>
2016-08-06 12:14:38 +0200

    Test with more params in annotation

---
+++ varnish-exec-spring/src/test/groovy/com/github/platan/varnishexec/spring/AnnotationMoreParamsSpec.groovy
@@ -0,0 +1,22 @@
+package com.github.platan.varnishexec.spring
+
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.boot.test.SpringApplicationContextLoader
+import org.springframework.boot.test.WebIntegrationTest
+import org.springframework.test.context.ContextConfiguration
+import spock.lang.Specification
+
+@ContextConfiguration(classes = [Application], loader = SpringApplicationContextLoader)
+@WebIntegrationTest(randomPort = true)
+@VarnishTest(configFile = './varnish/default.vcl', address = @VarnishTest.HostAndPort(host = 'localhost', port = 0), randomName = true,
+        managementAddress = @VarnishTest.HostAndPort(host = 'localhost', port = 10090), storage = 'malloc,1m', varnishdCommand = '/usr/sbin/varnishd')
+class AnnotationMoreParamsSpec extends Specification {
+
+    @Value('${local.varnish.port}')
+    def varnishPort
+
+    def "get resource via varnish"() {
+        expect:
+        "http://localhost:$varnishPort/".toURL().text == 'Hello World!'
+    }
+}

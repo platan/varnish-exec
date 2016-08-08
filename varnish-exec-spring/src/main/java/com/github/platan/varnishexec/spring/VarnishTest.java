b5e3bb22df9f4ef55ecf14e83a44b577d1a96b03
Marcin Mielnicki <marcin.mielnicki@gmail.com>
2016-08-08 22:30:29 +0200

    Documentaion for VarnishTest

--- varnish-exec-spring/src/main/java/com/github/platan/varnishexec/spring/VarnishTest.java
+++ varnish-exec-spring/src/main/java/com/github/platan/varnishexec/spring/VarnishTest.java
@@ -8,15 +8,30 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ * Test class annotation that is used to start varnish for tests.
+ */
 @Documented
 @Inherited
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.TYPE)
 public @interface VarnishTest {
+
+    /**
+     * Set a port to {@code 0} to trigger listening on a random port.
+     * <p>
+     * Default is {@code localhost:10080}.
+     */
     HostAndPort address() default @VarnishTest.HostAndPort(host = "localhost", port = 10080);
 
+    /**
+     * Only one of "backend" or "config file" can be specified.
+     */
     HostAndPort backend() default @HostAndPort(host = "", port = -1);
 
+    /**
+     * Only one of "backend" or "config file" can be specified.
+     */
     String configFile() default "";
 
     HostAndPort managementAddress() default @HostAndPort(host = "", port = -1);
@@ -29,6 +44,9 @@ public @interface VarnishTest {
 
     String varnishdCommand() default "";
 
+    /**
+     * Defines host and port of {@link VarnishTest#address()}, {@link VarnishTest#backend()} or {@link VarnishTest#managementAddress()}.
+     */
     @Retention(RetentionPolicy.RUNTIME)
     @Target({})
     @interface HostAndPort {
